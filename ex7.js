31350521de441dc1ad3799276728e6601a030f10
foram patel <p.foram147@gmail.com>
2021-06-25 15:06:37 +0100

    Update ex7.js

--- ex7.js
+++ ex7.js
@@ -1,7 +1,3 @@
-const reversString = function(str1){
-    let newString
-    for(let reversedString = str1.length - 1; reversedString >=0;reversedString++){
-    newString  = str1[reversedString];
 function reverseString(stringToReverse) {
     var splitedString = stringToReverse.split(""); 
      var reversedArray = splitedString.reverse();  
