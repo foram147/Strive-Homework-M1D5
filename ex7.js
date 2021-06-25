1f4e5bcb8f5b152b20cae3969757d480e53de186
foram patel <p.foram147@gmail.com>
2021-06-25 14:37:46 +0100

    ex7.js
    ex 7

---
+++ ex7.js
@@ -0,0 +1,7 @@
+const reversString = function(str1){
+    let newString
+    for(let reversedString = str1.length - 1; reversedString >=0;reversedString++){
+    newString  = str1[reversedString];
+}
+}
+console.log(reversString("abc"));
